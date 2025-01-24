6addc51618e385b9f0cbc07feb2993c02f00caf0
Jørgen Katralen <jorgen_nk@live.no>
2025-01-24 18:44:14 +0100

    🎨 F

--- scripts/generateShieldList.mjs
+++ scripts/generateShieldList.mjs
@@ -1,13 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const fs = require("fs");
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const dir = path.join(process.cwd(), "public", "kommunevapen");
-
 const files = fs.readdirSync(dir).filter((file) => file.endsWith(".svg"));
 
-const outputPath = path.join(process.cwd(), "public", "shields.json");
-fs.writeFileSync(outputPath, JSON.stringify(files, null, 2));
+fs.writeFileSync(
+  path.join(process.cwd(), "public", "shields.json"),
+  JSON.stringify(files, null, 2)
+);
 
 console.log("✅ Shield list generated successfully!");

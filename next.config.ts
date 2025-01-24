cae91e8a4e649cc6b0839e4232720e1208a872d0
Jørgen Katralen <jorgen_nk@live.no>
2025-01-24 18:05:25 +0100

    🔧 Configs for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Ensures static HTML export
+  basePath: "/holandsgata", // Update with your GitHub repo name
+  trailingSlash: true, // Ensures URLs work correctly
 };
 
-export default nextConfig;
+module.exports = nextConfig;

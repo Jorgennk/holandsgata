f9d0c3df055250b618a456bc8b468ec2671513d9
Jørgen Katralen <jorgen_nk@live.no>
2025-01-24 16:30:24 +0100

    🏗️ Github workflows and configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? "/holandsgata" : "",
+  basePath: isProd ? "/holandsgata" : "",
+  images: {
+    unoptimized: true, // Required for static exports
+  },
 };
-
-export default nextConfig;
